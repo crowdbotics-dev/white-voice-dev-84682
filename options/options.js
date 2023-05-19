470e989b5cdfd62d1ee30c161e8e6307bbc1379f
Crowdbotics <engineering@crowdbotics.com>
2023-05-19 12:32:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-voice-dev-84682",
+  url: "https://white-voice-dev-84682.botics.co",
+  api: "https://white-voice-dev-84682.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
